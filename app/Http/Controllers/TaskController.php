08c34f3bad86f957295f2a489cb5d0edf4ebf11a
alpaca1231 <kazuki.amatatsu.fd@gmail.com>
2024-01-26 16:31:48 +0900

    feat: タスク一覧表示

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,7 +13,9 @@ class TaskController extends Controller
      */
     public function index()
     {
-        return view('tasks.index');
+        $tasks = Task::all();
+        return view('tasks.index', compact('tasks'));
+        // return view('tasks.index', ['tasks' => $tasks]); こう書いてもOK
     }
 
     /**
