7e5770377175da691f2adf0e49767862a24b9987
alpaca1231 <kazuki.amatatsu.fd@gmail.com>
2024-01-26 17:00:27 +0900

    feat: タスク状態切り替え機能

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -13,7 +13,8 @@ class TaskController extends Controller
      */
     public function index()
     {
-        $tasks = Task::all();
+        // $tasks = Task::
+        $tasks = Task::where('status', false)->get();
         return view('tasks.index', compact('tasks'));
         // return view('tasks.index', ['tasks' => $tasks]); こう書いてもOK
     }
@@ -74,22 +75,36 @@ class TaskController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        $rules = [
-            'task_name' => 'required|max:100',
-        ];
+        // 「編集する」ボタンをおしたとき
+        if ($request->status === null) {
+            $rules = [
+                'task_name' => 'required|max:100',
+            ];
 
-        $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
+            $messages = ['required' => '必須項目です', 'max' => '100文字以下にしてください。'];
 
-        Validator::make($request->all(), $rules, $messages)->validate();
+            Validator::make($request->all(), $rules, $messages)->validate();
 
-        // 該当のタスクを検索
-        $task = Task::find($id);
+            // 該当のタスクを検索
+            $task = Task::find($id);
 
-        // モデル->カラム名 = 値 で、データを割り当てる
-        $task->name = $request->input('task_name');
+            // モデル->カラム名 = 値 で、データを割り当てる
+            $task->name = $request->input('task_name');
 
-        // データベースに保存
-        $task->save();
+            // データベースに保存
+            $task->save();
+        } else {
+            // 「完了」ボタンを押したとき
+
+            // 該当のタスクを検索
+            $task = Task::find($id);
+
+            // モデル->カラム名 = 値 で、データを割り当てる
+            $task->status = true; // true:完了、false:未完了
+
+            // データベースに保存
+            $task->save();
+        }
 
         // リダイレクト
         return redirect('/tasks');
